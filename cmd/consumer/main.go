4c7cf95b4d4e6b035adbc3f4cee8f6a98537a70d
Amit Suthar <sutharamit707@gmail.com>
2026-06-17 17:29:03 +0530

    fix: log consumer fixed to unmarshal log message

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"log"
 	"os"
 
 	shared "github.com/amitsuthar69/parsel/internal/consumer"
+	models "github.com/amitsuthar69/parsel/internal/models"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -21,10 +23,31 @@ func main() {
 
 	ctx := context.Background()
 	if err := rdb.Ping(ctx).Err(); err != nil {
-		fmt.Printf("Could not connect to Redis: %v", err)
+		log.Printf("Could not connect to Redis: %v", err)
 		os.Exit(1)
 	}
 
-	handler := func(msg redis.XMessage) {}
-	shared.StartConsumer(ctx, rdb, "parsel:logs", "group-1", "consumer-1", handler)
+	handler := func(msg redis.XMessage) {
+		raw, ok := msg.Values["logData"].(string)
+		if !ok {
+			log.Printf("type assertion failed for message %s", msg.ID)
+			return
+		}
+
+		var entry models.Log
+		if err := json.Unmarshal([]byte(raw), &entry); err != nil {
+			log.Printf("unmarshal failed for message %s: %v", msg.ID, err)
+			return
+		}
+
+		log.Printf("[%s] %s | %s: %s",
+			entry.Timestamp.Format("15:04:05"),
+			entry.Level,
+			entry.Service,
+			entry.Message,
+		)
+	}
+
+	log.Println("Logger consumer started...")
+	shared.StartConsumer(ctx, rdb, "parsel:logs", "logger-group", "logger-1", handler)
 }
